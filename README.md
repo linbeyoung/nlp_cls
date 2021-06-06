74637f9805a91c54866f733ea2460d4a483c33c4
Liu Feng <liufeng2900@gmail.com>
2021-06-06 14:07:53 +0800

    添加数据集读取方法

---
+++ README.md
@@ -0,0 +1,17 @@
+# 数据集读取
+
+```python
+from utils.data_loader import load_data
+
+train,test=load_data() # 即可读取数据集，数据集已经过shuffle
+```
+
+格式：
+
+```python
+[
+    ["中国将军团8年来首访日本 日方希望加强和中国沟通",1],
+    ["他一张脸都皱紧了",4],
+]
+```
+
---
+++ utils/data_loader.py
@@ -0,0 +1,40 @@
+import glob
+import numpy as np
+from math import floor
+
+raw_data_path="./小组作业-语料/"
+
+test_ratio=0.2 # 测试集比例
+seed=107
+
+def method_1():
+    """
+    划分方法一：所有数据打乱，按比例切割训练、测试集
+    默认测试集比例0.2
+    """
+    all_data=[]
+
+    for file in glob.glob(raw_data_path+"*"):
+        with open(file,encoding="utf-8") as f:
+            content=f.readlines()
+            for line in content:
+                all_data.append(line.rstrip().split('\t'))
+                
+    for index,line in enumerate(all_data):
+        all_data[index][1]=int(line[1]) # convert string to int
+    
+    np.random.seed(seed)
+    np.random.shuffle(all_data)
+    
+    split=floor(len(all_data)*(1-test_ratio))
+    train=all_data[:split]
+    test=all_data[split:]
+
+    print("加载完成，测试集比例%s"%(test_ratio))
+    print("训练集%s条"%(len(train)))
+    print("测试集%s条"%(len(test)))
+    
+    return train,test
+
+def load_data():
+    return method_1()
