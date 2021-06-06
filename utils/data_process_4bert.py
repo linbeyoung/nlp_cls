78d2c1970310324ab012947975bbc68e114bdb81
beyoung <linbeyoung@foxmail.com>
2021-06-06 20:45:34 +0800

    update data process

--- utils/data_process_4bert.py
+++ utils/data_process_4bert.py
@@ -20,18 +20,13 @@ def data_process_bert(file, outfile=None):
             sen, tag = line.split('\t')[0], line.split('\t')[1]
 
             with open(outfile, 'a', encoding='UTF-8') as of:
-                # print(tag, sen)
-                print(tag + '\t' + sen + '\n')
+                # print(tag + '\t' + sen + '\n')
                 of.writelines(tag + '\t' + sen + '\n')
 
     return
 
 
-# def get_all_files(dir):
-#     files = os.listdir(dir)
-#     for
-#     return
-root_dir = '小组作业-语料'
+root_dir = 'datasets/'
 files = os.listdir(root_dir)
 for file in files:
     if file.endswith('txt'):
