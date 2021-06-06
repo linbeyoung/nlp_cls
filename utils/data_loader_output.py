11af4ee9779564714ee7aa11d1e791c88c9f9952
beyoung <linbeyoung@foxmail.com>
2021-06-06 20:24:17 +0800

    modify

--- utils/data_loader_output.py
+++ utils/data_loader_output.py
@@ -1,36 +1,38 @@
 import glob
-import numpy as np
 from math import floor
 
-raw_data_path="./小组作业-语料/"
+import numpy as np
 
-test_ratio=0.2 # 测试集比例
-seed=107
+raw_data_path = "./小组作业-语料/"
 
-def method_1(OUTPUT = False):
+test_ratio = 0.2  # 测试集比例
+seed = 107
+
+
+def method_1(OUTPUT=False):
     """
     划分方法一：所有数据打乱，按比例切割训练、测试集
     默认测试集比例0.2
     """
-    all_data=[]
+    all_data = []
 
-    for file in glob.glob(raw_data_path+"*"):
-        with open(file,encoding="utf-8") as f:
-            content=f.readlines()
+    for file in glob.glob(raw_data_path + "*"):
+        with open(file, encoding="utf-8") as f:
+            content = f.readlines()
             for line in content:
                 # all_data.append(line.rstrip().split('\t'))
                 all_data.append(line.rstrip())
 
     # for index,line in enumerate(all_data):
     #     all_data[index][1]=int(line[1]) # convert string to int
-    
+
     np.random.seed(seed)
     np.random.shuffle(all_data)
-    
-    split=floor(len(all_data)*(1-test_ratio))
-    train=all_data[:split]
+
+    split = floor(len(all_data) * (1 - test_ratio))
+    train = all_data[:split]
     # print(train)
-    test=all_data[split:]
+    test = all_data[split:]
 
     if OUTPUT:
         with open('datasets/train.txt', 'a') as train_set:
@@ -41,13 +43,15 @@ def method_1(OUTPUT = False):
             for line in test:
                 test_set.writelines(line + '\n')
 
-    print("加载完成，测试集比例%s"%(test_ratio))
-    print("训练集%s条"%(len(train)))
-    print("测试集%s条"%(len(test)))
-    
-    return train,test
+    print("加载完成，测试集比例%s" % (test_ratio))
+    print("训练集%s条" % (len(train)))
+    print("测试集%s条" % (len(test)))
+
+    return train, test
+
 
 def load_data():
     return method_1()
 
-method_1(OUTPUT = True)
+
+method_1(OUTPUT=True)
